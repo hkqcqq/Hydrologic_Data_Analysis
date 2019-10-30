5abb33b5eaaf7d628976b67327a3e783c2e79642
Keqi He <kh382@duke.edu>
2019-10-30 14:54:15 -0400

    1030

--- try.R
+++ try.R
@@ -1,6 +1,11 @@
 library(dataRetrieval)
+library(tidyverse)
 
 huc1021 <- seq(10210001,10210010,1)
 huc1029 <- seq(10290101,10290110,1)
 
 hucsites1021 <- whatNWISdata(huc = huc1021)
+
+huc1021N <- hucsites1021 %>% filter(parm_cd == "00600")
+huc1021P <- hucsites1021 %>% filter(parm_cd == "00665")
+huc1021D <- hucsites1021 %>% filter(parm_cd == "00060", stat_cd == "00003")
